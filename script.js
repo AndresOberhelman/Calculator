350b573c20ec8dd28b297c136530f76b292de883
AndresOberhelman <andres.w.oberhelman@gmail.com>
2022-07-24 01:09:15 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+function multiply(firstNum,secondNum){
+result = firstNum*secondNum;
+return result
+}
+
+function add(firstNum,secondNum){
+    result = firstNum + secondNum;
+    return result
+}
+
+
+function divide(firstNum,secondNum){
+    result = firstNum / secondNum;
+    return result
+}
+
+function subtract(firstNum,secondNum){
+    result = firstNum - secondNum;
+    return result
+}
+
+function operate(firstNum,secondNum,operator){
+
+}
+
+console.log(subtract(9,3))
