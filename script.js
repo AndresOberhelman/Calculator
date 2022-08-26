786c2565be6f0c2438a6a45f35e8c65a0e13c4f9
AndresOberhelman <andres.w.oberhelman@gmail.com>
2022-08-26 11:40:17 -0500

    Javascript done

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const equal = document.querySelector(".equal");
 equal.addEventListener("click",calculate); // event listener to call calculate
 const numberButtons= document.querySelectorAll(".number");
 const clear = document.querySelector(".clear")
+clear.addEventListener("click",clearDisplay); // event listener to clear calculator
 const operators = document.querySelectorAll(".operator");
 
 numberButtons.forEach((btn) => { //an event listener to call the handleNum function when a number is clicked
@@ -18,10 +19,14 @@ numberButtons.forEach((btn) => { //an event listener to call the handleNum funct
 });
 
 function handleNum(number) {
-    if(currentNum.length<=15){ // add a limit of 15 characters to number
-        currentNum+=number;
-        currentDisplayNumber.textContent=currentNum;
-    }      
+    if (previousNum !== "" && currentNum !== "" && operator === "") {
+        previousNum = "";
+        currentDisplayNumber.textContent = currentNum;
+      }
+      if (currentNum.length <= 11) {
+        currentNum += number;
+        currentDisplayNumber.textContent = currentNum;
+      }
 }
 
 operators.forEach((btn) => { //an event listener to call the handleOperator function when a operator is clicked
@@ -30,26 +35,79 @@ operators.forEach((btn) => { //an event listener to call the handleOperator func
     });
 });
 
-function handleOperator(op){
-    operator = op;
-    previousNum=currentNum;
-    previousDisplayNumber.textContent = previousNum + " " + operator;
-    currentNum = "";
-    currentDisplayNumber.textContent = "";
+function handleOperator(op){ // function accepts operator input and changes currentNum to previousNum and currentNum to blank
+    if (previousNum === "") {
+        previousNum = currentNum;
+        operatorCheck(op);
+      } else if (currentNum === "") {
+        operatorCheck(op);
+      } else {
+        compute();
+        operator = op;
+        currentDisplayNumber.textContent = "0";
+        previousDisplayNumber.textContent = previousNum + " " + operator;
+      }
 }
 
-
-
+function operatorCheck(text) { // allows for operations on previous numbers
+    operator = text;
+    previousDisplayNumber.textContent = previousNum + " " + operator;
+    currentDisplayNumber.textContent = "0";
+    currentNum = "";
+  }
 
  function calculate(){
     previousNum = Number(previousNum);
     currentNum = Number(currentNum);
-   switch(operator){ //switch statement to handle different opperators
-    case "+":
-        previousNum+=currentNum;
-    case "x":
-        previousNum*=currentNum;
-   }
-      previousDisplayNumber.textContent=""
-      currentDisplayNumber.textContent=previousNum;
+    if (operator === "+") {
+        previousNum += currentNum;
+      } else if (operator === "-") {
+        previousNum -= currentNum;
+      } else if (operator === "x") {
+        previousNum *= currentNum;
+      } else if (operator === "/") {
+        if (currentNum <= 0) {
+          previousNum = "Error";
+          displayResults();
+          return;
+        }
+        previousNum /= currentNum;
+      }
+//    switch(operator){ //switch statement to handle different opperators
+//     case "+":
+//         previousNum += currentNum;
+//     case "x":
+//         previousNum *= currentNum;
+//     case "-":
+//         previousNum -= currentNum;
+//     case "/":
+//         previousNum /= currentNum;
+//    }
+    previousNum = roundNumber(previousNum);
+    previousNum = previousNum.toString();
+    displayResults();
   }
+
+  function roundNumber (num) { // rounds to 6th decimal
+    return Math.round(num*100000) / 100000;
+   }
+
+  function displayResults() { // allows larger numbers to be displayed up to 11 also display results
+    if (previousNum.length <= 11) {
+      currentDisplayNumber.textContent = previousNum;
+    } else {
+      currentDisplayNumber.textContent = previousNum.slice(0, 11) + "...";
+    }
+    previousDisplayNumber.textContent = "";
+    operator = "";
+    currentNum = "";
+  }
+
+  function clearDisplay(){
+    currentNum="";
+    previousNum="";
+    operator="";
+    currentDisplayNumber.textContent="0";
+    previousDisplayNumber.textContent="";
+  }
+
